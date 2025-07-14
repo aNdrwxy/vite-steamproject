3ab29f2ebe0f906c2f4606585acb6e7e20610d25
andrwxy <andrewparedesbravoxd@gmail.com>
2025-07-14 04:22:10 -0500

    Ahorasi

--- src/api/profiles.api.js
+++ src/api/profiles.api.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 export const createUser = (user) => { 
-    return axios.post(`${import.meta.env.VITE_API_URL}`, user);
+    return axios.post(`${import.meta.env.VITE_API_URL}accounts/api/v1/user/`, user);
 };
 
 export const getAllProfiles = () => {
-    return axios.get(`${import.meta.env.VITE_API_URL}`);
+    return axios.get(`${import.meta.env.VITE_API_URL}accounts/api/v1/profile/`);
 };
 
 export const getProfile = (id) => { 
-    return axios.get(`${import.meta.env.VITE_API_URL}${id}/`);
+    return axios.get(`${import.meta.env.VITE_API_URL}accounts/api/v1/profile/${id}/`);
 };
 
 
 export const updateProfile = (id, profile) => {
-    return axios.put(`${import.meta.env.VITE_API_URL}${id}/`, profile)
+    return axios.put(`${import.meta.env.VITE_API_URL}accounts/api/v1/profile/${id}/`, profile)
 };
 
