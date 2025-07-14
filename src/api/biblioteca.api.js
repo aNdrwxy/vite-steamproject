d9589a93dd5c60f34dde67c4e7ed3143d467271c
andrwxy <andrewparedesbravoxd@gmail.com>
2025-07-14 04:34:24 -0500

    axd

--- src/api/biblioteca.api.js
+++ src/api/biblioteca.api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const getGamerForId = (id) => {
-    return axios.get(`${import.meta.env.VITE_API_URL}biblioteca/api/v1/biblioteca/${id}/`);
+    return axios.get(`${import.meta.env.VITE_API_URLB}${id}/`);
 };
--- src/api/profiles.api.js
+++ src/api/profiles.api.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
-export const createUser = (user) => { 
-    return axios.post(`${import.meta.env.VITE_API_URL}accounts/api/v1/user/`, user);
+export const createUser = (user) => {
+    return axios.post(`${import.meta.env.VITE_API_URL}user/`, user);
 };
 
 export const getAllProfiles = () => {
-    return axios.get(`${import.meta.env.VITE_API_URL}accounts/api/v1/profile/`);
+    return axios.get(`${import.meta.env.VITE_API_URL}profile/`);
 };
 
 export const getProfile = (id) => { 
-    return axios.get(`${import.meta.env.VITE_API_URL}accounts/api/v1/profile/${id}/`);
+    return axios.get(`${import.meta.env.VITE_API_URL}profile/${id}/`);
 };
 
 
 export const updateProfile = (id, profile) => {
-    return axios.put(`${import.meta.env.VITE_API_URL}accounts/api/v1/profile/${id}/`, profile)
+    return axios.put(`${import.meta.env.VITE_API_URL}profile/${id}/`, profile)
 };
 
